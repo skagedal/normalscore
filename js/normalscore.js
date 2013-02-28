b237b992b183a937d63caf3013f3472a6d1d20f7
Simon Kågedal Reimer <skagedal@gmail.com>
2013-02-28 22:42:53 +0100

    Add T-score.

--- js/normalscore.js
+++ js/normalscore.js
@@ -6,6 +6,9 @@ function toIQ(z) { return z * 15 + 100; }
 function fromZ(z) { return z; }
 function toZ(z) { return z; }
 
+function fromT(t) { return (t - 50) / 10; }
+function toT(z) { return z * 10 + 50; }
+
 function fromPerc(perc) { return jStat.normal.inv(perc / 100, 0, 1); }
 function toPerc(z) { return jStat.normal.cdf(z, 0, 1) * 100; }
 
@@ -29,11 +32,13 @@ function addScore(z) {
     var normal = roundNumber(z, 2);
     var perc = roundNumber(toPerc(z), 2);
     var iq = roundNumber(toIQ(z), 1);
+    var t = roundNumber(toT(z), 1);
     var stanine = toStanine(z);
     var sta19 = toSta19(z);
 
     $("#outputHeader").after("<tr><td>" + normal + "</td><td>" + 
 			     perc + "</td><td>" + iq + "</td><td>" + 
+			     t + "</td><td>" +
 			     stanine + "</td><td>" + sta19 + "</td></tr>");
 
     var colors = ["#ff0000", "#ff8888", "#ffcccc", "#ffeeee"];
@@ -54,8 +59,8 @@ function addScore(z) {
 
 function updateTooltip(z) {
     $("#tooltip").html(sprintf(
-	"Z=%.3f, Perc=%.2f, IQ=%.1f, Stanine=%d, Standard 19=%d", 
-	z, toPerc(z), toIQ(z), toStanine(z), toSta19(z)));
+	"Z=%.3f, Perc=%.2f, IQ=%.1f, T=%.1f, Stanine=%d, Standard 19=%d", 
+	z, toPerc(z), toIQ(z), toT(z), toStanine(z), toSta19(z)));
 }
 
 $(document).ready(function() {
@@ -101,6 +106,9 @@ $(document).ready(function() {
 	    break;
 	case 'iq':
 	    addScore(fromIQ(score));
+	    break;
+	case 't':
+	    addScore(fromT(score));
 	    break;
 	case 'perc':
 	    addScore(fromPerc(score));
